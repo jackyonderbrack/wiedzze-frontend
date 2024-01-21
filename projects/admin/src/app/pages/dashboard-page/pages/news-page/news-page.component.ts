e4f6374d593169a7348da43cfe7b611664893331
Michał Łuczak <mike.projektowanie@gmail.com>
2024-01-21 19:16:11 +0100

    add post component added

--- projects/admin/src/app/pages/dashboard-page/pages/news-page/news-page.component.ts
+++ projects/admin/src/app/pages/dashboard-page/pages/news-page/news-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AddPostFormComponent } from '../../components/add-post-form/add-post-form.component';
 
 @Component({
     selector: 'app-news-page',
     templateUrl: './news-page.component.html',
     styleUrls: ['./news-page.component.scss'],
-    standalone: true
+    standalone: true,
+    imports: [AddPostFormComponent]
 })
 export class NewsPageComponent {}
