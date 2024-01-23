58bfc4749b5ed3c9250976ae64fd1f78f1e50bf3
jackyonderbrack <mike.projektowanie@gmail.com>
2024-01-23 14:31:16 +0100

    feat: category crud added

--- projects/admin/src/app/pages/dashboard-page/components/category-list/category-list.component.ts
+++ projects/admin/src/app/pages/dashboard-page/components/category-list/category-list.component.ts
@@ -1,21 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
+import { MatListModule } from '@angular/material/list';
 import { CategoryService } from 'projects/admin/src/app/services/category/category.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
     selector: 'app-category-list',
     templateUrl: './category-list.component.html',
     styleUrls: ['./category-list.component.scss'],
     standalone: true,
-    imports: [MatTableModule, MatButtonModule]
+    imports: [
+        CommonModule,
+        MatListModule,
+        MatButtonModule,
+        MatInputModule,
+        FormsModule,
+        MatFormFieldModule,
+        ReactiveFormsModule,
+        MatIconModule,
+        MatCardModule
+    ]
 })
 export class CategoryListComponent implements OnInit {
-    displayedColumns: string[] = ['categoryName', 'categoryOptions'];
-
-    public categoryListArray = [];
-    static categoryListArray: any;
+    public categoryListArray: any = [];
+    selectedCategory: any = null;
 
     constructor(
         private categoryService: CategoryService,
@@ -55,5 +69,41 @@ export class CategoryListComponent implements OnInit {
         }
     }
 
-    handleUpdateCategory(id: any) {}
+    handleUpdateCategory(category: any) {
+        console.log('Aktualna kategoria do edycji: ', category);
+        this.selectedCategory = { ...category };
+    }
+
+    submitUpdatedCategory() {
+        if (this.selectedCategory) {
+            this.categoryService
+                .updateCategory(
+                    this.selectedCategory,
+                    this.selectedCategory._id
+                )
+                .subscribe({
+                    next: () => {
+                        this.snackBar.open(
+                            'Kategoria zaktualizowana',
+                            'Zamknij',
+                            { duration: 3000 }
+                        );
+                        this.loadCategories();
+                        this.selectedCategory = null;
+                    },
+                    error: () => {
+                        this.snackBar.open(
+                            'Błąd podczas aktualizacji kategorii',
+                            'Zamknij',
+                            { duration: 3000 }
+                        );
+                        this.selectedCategory = null;
+                    }
+                });
+        }
+    }
+
+    cancelUpdatedCategory() {
+        this.selectedCategory = null;
+    }
 }
