b90f42adf1e067c2ccc41ab9b55d23badced0d5c
jackyonderbrack <mike.projektowanie@gmail.com>
2024-01-27 11:32:51 +0100

    feat section styling, build: section and list components building

--- projects/blackboard/src/app/pages/home-page/home-page.component.ts
+++ projects/blackboard/src/app/pages/home-page/home-page.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SectionComponent } from '../../components/section/section.component';
+import { ListComponent } from '../../components/list/list.component';
 
 @Component({
-  selector: 'app-home-page',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+    selector: 'app-home-page',
+    standalone: true,
+    imports: [CommonModule, SectionComponent, ListComponent],
+    templateUrl: './home-page.component.html',
+    styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent {
-
-}
+export class HomePageComponent {}
