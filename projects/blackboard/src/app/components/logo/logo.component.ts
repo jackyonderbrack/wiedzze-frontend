3fa889a191e2f3476c09b1477fc6da236751b114
jackyonderbrack <mike.projektowanie@gmail.com>
2024-01-29 20:17:30 +0100

    style: add logo

--- projects/blackboard/src/app/components/logo/logo.component.ts
+++ projects/blackboard/src/app/components/logo/logo.component.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-logo',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './logo.component.html',
-  styleUrls: ['./logo.component.scss']
+    selector: 'app-logo',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './logo.component.html',
+    styleUrls: ['./logo.component.scss']
 })
-export class LogoComponent {
-
-}
+export class LogoComponent {}
