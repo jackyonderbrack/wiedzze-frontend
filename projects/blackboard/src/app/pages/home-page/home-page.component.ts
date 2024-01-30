8ab2536919b642625afdf4a43185e6dcd8a5dca7
jackyonderbrack <mike.projektowanie@gmail.com>
2024-01-30 12:00:46 +0100

    style: section and card styling

--- projects/blackboard/src/app/pages/home-page/home-page.component.ts
+++ projects/blackboard/src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SectionComponent } from '../../components/section/section.component';
 import { ListComponent } from '../../components/list/list.component';
 import { TitleComponent } from '../../components/title/title.component';
 import { PostsComponent } from '../../components/posts/posts.component';
+import { Post } from '../../models/post.model';
+import { NewsService } from 'projects/admin/src/app/services/news/news.service';
 
 @Component({
     selector: 'app-home-page',
@@ -18,4 +20,24 @@ import { PostsComponent } from '../../components/posts/posts.component';
     templateUrl: './home-page.component.html',
     styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent {}
+export class HomePageComponent implements OnInit {
+    groupedPosts: Map<string, Post[]> = new Map();
+
+    constructor(private postsService: NewsService) {}
+
+    ngOnInit(): void {
+        this.postsService.getAllNews().subscribe((posts) => {
+            this.groupPostsByCategory(posts);
+        });
+    }
+
+    groupPostsByCategory(posts: Post[]) {
+        posts.forEach((post) => {
+            const categoryName = post.newsCategory.categoryName;
+            if (!this.groupedPosts.has(categoryName)) {
+                this.groupedPosts.set(categoryName, []);
+            }
+            this.groupedPosts.get(categoryName)?.push(post);
+        });
+    }
+}
